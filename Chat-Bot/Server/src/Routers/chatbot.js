1c4c6c0dce4710bcc3511575e5ee076dde1350e5
Sukhi898 <146666624+Sukhi898@users.noreply.github.com>
2024-08-02 17:39:43 +0530

    Delete Chat-Bot directory

--- Chat-Bot/Server/src/Routers/chatbot.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { fetchForm, saveChat } = require("../Controllers/Chat");
-
-router.get("/form/unique/:id", fetchForm);
-router.post("/save-chat", saveChat);
-
-module.exports = router;
